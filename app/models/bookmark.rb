93c2d954382d9c1438fdf653423a2ddfea9769a2
Vinicius Vinci <vvinci@protonmail.com>
2022-04-30 14:41:37 -0300

    models done

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie, :list, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates_uniqueness_of :movie, scope: :list
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_152450) do
+ActiveRecord::Schema.define(version: 2022_04_30_154407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
-    t.bigint "bookmark_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["bookmark_id"], name: "index_lists_on_bookmark_id"
   end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "overview"
     t.string "poster_url"
-    t.integer "raing"
-    t.bigint "bookmark_id", null: false
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["bookmark_id"], name: "index_movies_on_bookmark_id"
   end
 
-  add_foreign_key "lists", "bookmarks"
-  add_foreign_key "movies", "bookmarks"
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
